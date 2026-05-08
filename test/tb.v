f905502c25d937b8af3fd6d458e7614e378425a3
chainsofheaven23 <himadrighosh70@gmail.com>
2026-05-08 23:13:10 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_unified_error_detection (
+  tt_um_unified_error_detection error_detect(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
